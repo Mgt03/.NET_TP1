bae97d7b615b4117b71555d5de44f59bd0e820b3
Mgt03 <max.g03@outlook.fr>
2023-06-03 08:44:03 +0200

    ajout query/command

---
+++ BusinessLogicLayer/Commands/EmployeCommand.cs
@@ -0,0 +1,51 @@
+﻿using Bibliotheque;
+using Bibliotheque.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Commands
+{
+    class EmployeCommand
+    {
+        private readonly Context context;
+
+        public EmployeCommand(Context context)
+        {
+            this.context = context;
+        }
+
+        public int Ajouter(Employe employe)
+        {
+            context.Employes.Add(employe);
+            return context.SaveChanges();
+        }
+
+        public int Modifier(Employe employe)
+        {
+            Employe emp = context.Employes.Where(e => e.Id == employe.Id).FirstOrDefault();
+            if(emp != null)
+            {
+                emp.Nom = employe.Nom;
+                emp.Prenom = employe.Prenom;
+                emp.Experiences = employe.Experiences;
+                emp.DateNaissance = employe.DateNaissance;
+                emp.Biographie = employe.Biographie;
+                emp.Anciennete = employe.Anciennete;
+            }
+            return context.SaveChanges();
+        }
+
+        public int Supprimer(Employe employe)
+        {
+            Employe emp = context.Employes.Where(e => e.Id == employe.Id).FirstOrDefault();
+            if(emp != null)
+            {
+                context.Employes.Remove(emp);
+            }
+            return context.SaveChanges();
+        }
+    }
+}
