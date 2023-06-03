9cf92272313fd1b052520eb9cb106c2f9c699951
Nyamort <rmurat.murat@gmail.com>
2023-06-03 08:35:41 +0200

    fix

--- BusinessLogicLayer/Queries/EmployeQuery.cs
+++ BusinessLogicLayer/Queries/EmployeQuery.cs
@@ -1,10 +1,6 @@
-﻿using Bibliotheque;
+using Bibliotheque;
 using Bibliotheque.Entity;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.Queries
 {
